6046798c4f7373eb4709bddedc43253fe2dcdf9b
Vitalii Kudriavets <14039142+VitaliiKudriavets@users.noreply.github.com>
2024-03-05 21:39:10 +0200

    Removed multiple files and added main.go

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	pointers()
+}
+
+func pointers() {
+	a := "hello"
+	b := 10
+	fmt.Println(a, b)
+	c := &a
+	fmt.Println(a, c)
+	*c = "world"
+	fmt.Println(a, *c)
+	d := &b
+	*d = *d / 2
+	fmt.Println(b)
 }
