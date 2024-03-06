2d4df0bf7b75b049f9bdd4aed82d9c42b125560a
Vitalii Kudriavets <14039142+VitaliiKudriavets@users.noreply.github.com>
2024-03-06 17:38:29 +0200

    Add movement methods for Point and callback functions

--- main.go
+++ main.go
@@ -10,12 +10,52 @@ type Point struct {
 func (p *Point) method() {
 	fmt.Println(p.X, p.Y)
 }
+func (p Point) movePointMethod(x, y int) Point {
+	p.X += x
+	p.Y += y
+	return p
+}
+func (p *Point) movePointMethodPtr(x, y int) {
+	p.X += x
+	p.Y += y
+}
 
 func main() {
 	pointers()
 	structs()
 	slicesAndArrays()
 	maps()
+
+	sumCallback := func(n1, n2 int) int {
+		return n1 + n2
+	}
+	result := doSomething(sumCallback, "sum")
+	fmt.Println(result)
+
+	multiplyCallback := func(n1, n2 int) int {
+		return n1 * n2
+	}
+	result = doSomething(multiplyCallback, "multiply")
+	fmt.Println(result)
+
+	orderPrice := totalPrice(1) //sum = 1
+	fmt.Println(orderPrice(1))  // sum = 1+1 closure
+	fmt.Println(orderPrice(2))  // sum = 1+1+2 closure
+
+	p := Point{X: 1, Y: 2}
+	fmt.Println(p)
+	fmt.Println(movePoint(p, 1, 1))
+	fmt.Println(p)
+	movePointPtr(&p, 1, 1)
+	fmt.Println(p)
+	p.movePointMethod(1, 5)
+	fmt.Println(p)
+	p.movePointMethodPtr(1, 5)
+	fmt.Println(p)
+	v := &p
+	v.movePointMethodPtr(1, 1)
+	fmt.Println(p)
+
 }
 
 func pointers() {
@@ -63,4 +103,25 @@ func maps() {
 	} else {
 		fmt.Println("value doesn't exist")
 	}
+}
+func doSomething(callback func(int, int) int, s string) int {
+	result := callback(5, 1)
+	fmt.Print(s, " ")
+	return result
+} // doSomething is callback function
+func totalPrice(initPrice int) func(int) int {
+	sum := initPrice
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+} // totalPrice is a closure
+func movePoint(p Point, x, y int) Point {
+	p.X += x
+	p.Y += y
+	return p
+}
+func movePointPtr(p *Point, x, y int) {
+	p.X += x
+	p.Y += y
 }
